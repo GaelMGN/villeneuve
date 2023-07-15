7bedef3310bf8f15a49ff2230b81288ebe05cdc1
Gaël Moigne <gmoigne@live.fr>
2023-07-15 20:25:29 +0200

    feat: TVA handling

--- src/components/CartPage/Cart.tsx
+++ src/components/CartPage/Cart.tsx
@@ -1,68 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Item } from '../../types/types';
-import { Box, TableHead, TextField, Typography } from '@mui/material';
-import { replaceUnderscores } from '../../utils/stringUtils';
-import Table from '@mui/material/Table';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import { setItemQuantity } from '../../reducers/cart';
+// redux
+import { useSelector } from 'react-redux';
+
+// mui
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+// components
+import { DataGrid } from './DataGrid';
 
 export const Cart = () => {
-  // import redux actions
-  const dispatch = useDispatch();
-
   const cartItems = useSelector((state: any) => state.cart.items);
-
-  type Props = {
-    items: Item[];
-  };
-
-  const DataGrid = (props: Props) => {
-    const { items } = props;
-    // create a Table Row for each item in the cart with Mui Table
-
-    const handleChange = (value: number, name: string) => {
-      console.log(value);
-      dispatch(setItemQuantity({ name, quantity: value }));
-    };
-
-    return (
-      <Box display='flex' width='60%' sx={{ border: '1px solid black' }}>
-        <Table>
-          {/* Table Head */}
-          <TableHead>
-            <TableRow>
-              <TableCell>Nom</TableCell>
-              <TableCell>Quantité</TableCell>
-              <TableCell>Prix unitaire</TableCell>
-              <TableCell>Prix total</TableCell>
-            </TableRow>
-          </TableHead>
-          {/* Table Body */}
-          {items.map((item: Item) => {
-            return (
-              <TableRow sx={{ bgcolor: 'lightblue' }}>
-                <TableCell>{replaceUnderscores(item.name)}</TableCell>
-                <TableCell>
-                  <TextField
-                    type='number'
-                    size='small'
-                    // make the width of the input smaller
-                    sx={{ width: '5rem' }}
-                    inputProps={{ min: 1 }}
-                    value={item.quantity}
-                    onChange={(e) => handleChange(Number(e.target.value), item.name)}
-                  />
-                </TableCell>
-                <TableCell>{item.price} €</TableCell>
-                <TableCell>{(item.price * item.quantity).toFixed(2)} €</TableCell>
-              </TableRow>
-            );
-          })}
-        </Table>
-      </Box>
-    );
-  };
 
   return (
     <Box
---
+++ src/components/CartPage/DataGrid.tsx
@@ -0,0 +1,85 @@
+// redux
+import { useDispatch } from 'react-redux';
+import { setItemQuantity } from '../../reducers/cart';
+import { clearItemFromCart } from '../../reducers/cart';
+
+// types
+import { Item } from '../../types/types';
+
+// MUI imports
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+// utils
+import { replaceUnderscores } from '../../utils/stringUtils';
+
+type Props = {
+  items: Item[];
+};
+
+export const DataGrid = (props: Props) => {
+  const { items } = props;
+
+  const dispatch = useDispatch();
+
+  const handleChange = (value: number, name: string) => {
+    console.log(value);
+    dispatch(setItemQuantity({ name, quantity: value }));
+  };
+
+  return (
+    <Box display='flex' width='60%' sx={{ border: '1px solid black' }}>
+      <Table>
+        {/* Table Head */}
+        <TableHead>
+          <TableRow>
+            <TableCell>Nom</TableCell>
+            <TableCell>Quantité</TableCell>
+            <TableCell>Prix unitaire HT</TableCell>
+            <TableCell>Prix total HT</TableCell>
+            <TableCell>TVA</TableCell>
+            <TableCell>Prix total TTC</TableCell>
+            <TableCell>Supprimer</TableCell>
+          </TableRow>
+        </TableHead>
+        {/* Table Body */}
+        {items.map((item: Item) => {
+          const priceTimesQuantity = Number((item.price * item.quantity).toFixed(2));
+          return (
+            <TableRow sx={{ bgcolor: 'lightblue' }}>
+              <TableCell>{replaceUnderscores(item.name)}</TableCell>
+              <TableCell>
+                <TextField
+                  type='number'
+                  size='small'
+                  // make the width of the input smaller
+                  sx={{ width: '5rem' }}
+                  inputProps={{ min: 1 }}
+                  value={item.quantity}
+                  onChange={(e) => handleChange(Number(e.target.value), item.name)}
+                />
+              </TableCell>
+              <TableCell>{item.price} €</TableCell>
+              <TableCell>{priceTimesQuantity} €</TableCell>
+              <TableCell>{((priceTimesQuantity / 100) * 20).toFixed(2)} €</TableCell>
+              <TableCell>{((priceTimesQuantity / 100) * 120).toFixed(2)} €</TableCell>
+              <TableCell>
+                <Button
+                  variant='contained'
+                  color='error'
+                  onClick={() => dispatch(clearItemFromCart(item.name))}>
+                  X
+                </Button>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </Table>
+    </Box>
+  );
+};
