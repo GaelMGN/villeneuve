31ec3b0596ce7cc814d7328e69e37a591bdadd4c
Gaël Moigne <gmoigne@live.fr>
2023-07-15 14:44:24 +0200

    feat: all slice OK

--- src/reducers/cart.ts
+++ src/reducers/cart.ts
@@ -10,11 +10,31 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    /**
+     * This function adds an item to the cart.
+     * @param state
+     * @param action
+     */
     addItemToCart: (state, action) => {
       state.items.push(action.payload);
+    },
+    /**
+     * This function removes an item from the cart.
+     */
+    removeItemFromCart: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
+    },
+    /**
+     * This function clears the cart.
+     * @param state
+     * @param action
+     */
+    clearCart: (state, action) => {
+      state.items = [];
     },
   },
 });
 
+// export the action creator
 export const { addItemToCart } = cartSlice.actions;
 export default cartSlice.reducer;
